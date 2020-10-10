6ce2f1e39ca022edd681e5c25bea44eb39ed7d7d
adamw <adam@warski.org>
2020-10-10 19:56:12 +0200

    Languages

--- backend/src/main/scala/com/softwaremill/bootzooka/MainModule.scala
+++ backend/src/main/scala/com/softwaremill/bootzooka/MainModule.scala
@@ -6,6 +6,7 @@ import cats.data.NonEmptyList
 import com.softwaremill.bootzooka.email.EmailModule
 import com.softwaremill.bootzooka.http.{Http, HttpApi}
 import com.softwaremill.bootzooka.infrastructure.InfrastructureModule
+import com.softwaremill.bootzooka.languages.LanguagesModule
 import com.softwaremill.bootzooka.metrics.MetricsModule
 import com.softwaremill.bootzooka.passwordreset.PasswordResetModule
 import com.softwaremill.bootzooka.security.SecurityModule
@@ -13,8 +14,7 @@ import com.softwaremill.bootzooka.user.UserModule
 import com.softwaremill.bootzooka.util.{DefaultIdGenerator, IdGenerator, ServerEndpoints}
 import monix.eval.Task
 
-/**
-  * Main application module. Depends on resources initialised in [[InitModule]].
+/** Main application module. Depends on resources initialised in [[InitModule]].
   */
 trait MainModule
     extends SecurityModule
@@ -22,14 +22,15 @@ trait MainModule
     with UserModule
     with PasswordResetModule
     with MetricsModule
-    with InfrastructureModule {
+    with InfrastructureModule
+    with LanguagesModule {
 
   override lazy val idGenerator: IdGenerator = DefaultIdGenerator
   override lazy val clock: Clock = Clock.systemUTC()
 
   lazy val http: Http = new Http()
 
-  private lazy val endpoints: ServerEndpoints = userApi.endpoints concatNel passwordResetApi.endpoints
+  private lazy val endpoints: ServerEndpoints = userApi.endpoints concatNel passwordResetApi.endpoints concatNel languagesApi.endpoints
   private lazy val adminEndpoints: ServerEndpoints = NonEmptyList.of(metricsApi.metricsEndpoint, versionApi.versionEndpoint)
 
   lazy val httpApi: HttpApi = new HttpApi(http, endpoints, adminEndpoints, collectorRegistry, config.api)
